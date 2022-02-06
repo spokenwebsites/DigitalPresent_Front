0d1366b95c59142b59d9ec8cb955240288a899ec
sukeshgandham <sukesh.gandham@gmail.com>
2022-02-06 18:55:14 -0500

    Updated files
    Updated the files code

--- src/app/viz1/viz1.component.ts
+++ src/app/viz1/viz1.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+declare function get_condegram_json(json): any;
 
 @Component({
   selector: 'app-viz1',
@@ -7,15 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Viz1Component implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient){
+    this.http.get<any>('assets/js/Spiralcondegram_final.json')
+    .subscribe(
+        data => {      
+          get_condegram_json(data);
+        },
+        error => {
+            console.log(error);
+        }
+    );
+  }
+  
   ngOnInit(): void {
-    /*this.loadJsFile("assets/js/Condegram_main.js");
+    this.loadJsFile("assets/js/d3.min.js");
+    this.loadJsFile("assets/js/d3.v4.js")
+   
+     this.loadJsFile("assets/js/Condegram_main.js");
   }
   public loadJsFile(url) {
     let node = document.createElement('script');
     node.src = url;
     node.type = 'text/javascript';
-    document.getElementsByTagName('head')[0].appendChild(node);*/
+    document.getElementsByTagName('head')[0].appendChild(node);
   }
 }
--- src/app/viz2/viz2.component.ts
+++ src/app/viz2/viz2.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import edge_bundlingJson from '../../assets/js/Topten.json';
+import { HttpClient } from "@angular/common/http";
+// import { stat } from 'fs';
+declare function get_edge_bund_json(json): any;
+
+ interface RootObject {
+  name: string;
+  type: string;
+  imports: string[];
+}
 
 @Component({
   selector: 'app-viz2',
   templateUrl: './viz2.component.html',
   styleUrls: ['./viz2.component.css']
 })
+
+
+
 export class Viz2Component implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
+    this.loadJsFile("assets/js/d3.min.js");
+    this.loadJsFile("assets/js/d3.v4.js")
+    
+     //this.loadJsFile("assets/js/Topten.json")
+   //  console.log(this.Events)
+  }
+  public events: Array<object> = [];
+  constructor(private http: HttpClient){
+    this.http.get<any>('assets/js/Topten.json')
+    .subscribe(
+        data => {      
+            get_edge_bund_json(data);
+        },
+        error => {
+            console.log(error);
+        }
+    );
+  }
+  public loadJsFile(url) {
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
   }
 
 }
